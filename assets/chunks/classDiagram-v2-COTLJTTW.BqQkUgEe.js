e6bff78db54394c70bf569fc24f2373c3eb6479f
shuji-bonji <shuji-bonji@users.noreply.github.com>
2025-04-19 23:24:45 +0000

    deploy: 2078646ea0bf82286acd7cd3b2b14efed4b0c62f

---
+++ assets/chunks/classDiagram-v2-COTLJTTW.BqQkUgEe.js
@@ -0,0 +1,1 @@
+import{s as a,c as s,a as e,C as t}from"./chunk-A2AXSNBT.Xd4gu-C2.js";import{_ as i}from"../app.DcK282dk.js";import"./chunk-RZ5BOZE2.DD_Opae9.js";import"./framework.M5Qft685.js";import"./theme.BJEbOMR9.js";var p={parser:e,get db(){return new t},renderer:s,styles:a,init:i(r=>{r.class||(r.class={}),r.class.arrowMarkerAbsolute=r.arrowMarkerAbsolute},"init")};export{p as diagram};
